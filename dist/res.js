359a956ea606299d2a0b64f8431090c6c80d1697
sahil singh <sahilsingh1413@gmail.com>
2024-04-04 22:13:06 +0530

    Updated the logic scroll first then alert

--- dist/res.js
+++ dist/res.js
@@ -1,32 +1,47 @@
-let contactme = document.getElementById('cb');
-let aboutme = document.getElementById('abb');
-let academic = document.getElementById('ab');
-let skills = document.getElementById('sb');
-let project = document.getElementById('pb');
-let expertise = document.getElementById('eb');
+// let contactme = document.getElementById('cb');
+// let aboutme = document.getElementById('abb');
+// let academic = document.getElementById('ab');
+// let skills = document.getElementById('sb');
+// let project = document.getElementById('pb');
+// let expertise = document.getElementById('eb');
 
 
+function scrollIntoElement(elementId){
+    const element = document.getElementById(elementId);
 
-contactme.onclick = ()=>{
-    confirm("You got it! if not - look at left ");
+    if(element){
+        element.scrollIntoView({behavior:"smooth"});
+    }
 }
 
-aboutme.onclick = ()=>{
-    confirm("You got it! if not - look at left ");
+function eventOnclick(elementId){
+    scrollIntoElement(elementId);//To scroll to the element first then apply alert function
+
+    setTimeout(()=>{
+        switch(elementId){
+        case 'cb':
+        case 'sb':
+        case 'eb':
+        case 'abb':
+            alert("You got it! , If not- look at left side");
+            break;
+        
+        case 'pb':
+        case 'ab':
+            alert("You got it! , If not - look at right side");
+            break;
+        
+        default:
+            break;
+        }
+    },1000)
+    
+
 }
 
-academic.onclick = ()=>{
-    confirm("You got it! if not - look at right ");
-}
-
-skills.onclick = ()=>{
-    confirm("You got it! if not - look at left ");
-}
-
-project.onclick = ()=>{
-    confirm("You got it! if not - look at right ");
-}
-
-expertise.onclick = ()=>{
-    confirm("You got it! if not - look at left ");
-}
+document.getElementById('cb').onclick = ()=> eventOnclick('cb');
+document.getElementById('sb').onclick = ()=> eventOnclick('sb');
+document.getElementById('eb').onclick = ()=> eventOnclick('eb');
+document.getElementById('abb').onclick = ()=> eventOnclick('abb');
+document.getElementById('pb').onclick = ()=> eventOnclick('pb');
+document.getElementById('ab').onclick = ()=> eventOnclick('ab');
