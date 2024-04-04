561e025e1884cf27cb751a8fcf92c94f9bb7257e
sahil singh <sahilsingh1413@gmail.com>
2024-04-04 15:47:24 +0530

    My Resume

---
+++ dist/res.js
@@ -0,0 +1,32 @@
+let contactme = document.getElementById('cb');
+let aboutme = document.getElementById('abb');
+let academic = document.getElementById('ab');
+let skills = document.getElementById('sb');
+let project = document.getElementById('pb');
+let expertise = document.getElementById('eb');
+
+
+
+contactme.onclick = ()=>{
+    confirm("You got it! if not - look at left ");
+}
+
+aboutme.onclick = ()=>{
+    confirm("You got it! if not - look at left ");
+}
+
+academic.onclick = ()=>{
+    confirm("You got it! if not - look at right ");
+}
+
+skills.onclick = ()=>{
+    confirm("You got it! if not - look at left ");
+}
+
+project.onclick = ()=>{
+    confirm("You got it! if not - look at right ");
+}
+
+expertise.onclick = ()=>{
+    confirm("You got it! if not - look at left ");
+}
